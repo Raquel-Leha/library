d08eeb635d4217d98772ca24c3799e053b3ff386
Raquel_Leha <rlopezn833@gmail.com>
2023-06-14 15:33:53 +0200

    library y servidor

---
+++ servidor/index.js
@@ -0,0 +1,23 @@
+
+
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require("cors");
+
+
+//Creamos el servidor
+const app = express();
+
+//Nos conectamos a la base de datos
+conectarDB();
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/books', require('./routes/book'));
+
+
+
+app.listen(4000, () => {
+    console.log("El servidor esta funcionando correctamente.")
+});
